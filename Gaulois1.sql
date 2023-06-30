c2278c5534f30b7ed7753939ede09b81b0a3edb5
derdeche <foued.derdeche@gmail.com>
2023-06-30 15:39:02 +0200

    affichage desc par lieu

--- Gaulois1.sql
+++ Gaulois1.sql
@@ -1,4 +1,7 @@
 /*Nom des lieux qui finissent par 'um'*/
 SELECT * FROM lieu WHERE nom_lieu LIKE '%um';
 
-
+/*Nombre de personnages par lieu (trié par nombre de personnages décroissant)*/
+SELECT COUNT(id_personnage), id_lieu  FROM personnage
+GROUP BY id_lieu
+ORDER BY  COUNT(id_personnage) DESC
