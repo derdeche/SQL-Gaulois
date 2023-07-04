ba5532e61250cf780680c796ed74d939a78369f8
derdeche <foued.derdeche@gmail.com>
2023-07-04 11:31:51 +0200

    nombre de personnes pour chaque specialité

--- Gaulois1.sql
+++ Gaulois1.sql
@@ -16,3 +16,10 @@ FROM personnage p
 INNER JOIN specialite s ON p.id_specialite =s.id_specialite
 INNER JOIN lieu l ON p.id_lieu= l.id_lieu
 ORDER BY l.nom_lieu ASC, p.nom_personnage ASC
+
+/*Nom des spécialités avec nombre de personnages par spécialité (trié par nombre de personnages décroissant).*/
+SELECT nom_specialite, COUNT(nom_personnage) AS nbPersonnages
+FROM personnage p
+INNER JOIN specialite s on p.id_specialite = s.id_specialite
+GROUP BY s.id_specialite
+ORDER BY nbPersonnages DESC
