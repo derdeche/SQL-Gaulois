34376864954f3b5d5870330694fe3ff920ec4f26
derdeche <foued.derdeche@gmail.com>
2023-07-04 10:43:17 +0200

    affichage des noms personnes par lieu

--- Gaulois1.sql
+++ Gaulois1.sql
@@ -11,3 +11,8 @@ GROUP BY  l.id_lieu
 ORDER BY  nbPersonnages DESC
 
 /*Nom des personnages + spécialité + adresse et lieu d'habitation, triés par lieu puis par nom de personnage*/
+SELECT nom_personnage,nom_specialite,adresse_personnage,nom_lieu
+FROM personnage p
+INNER JOIN specialite s ON p.id_specialite =s.id_specialite
+INNER JOIN lieu l ON p.id_lieu= l.id_lieu
+ORDER BY l.nom_lieu ASC, p.nom_personnage ASC
