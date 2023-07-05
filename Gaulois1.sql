b4cdbf5db84e6c112f5adfb50386c138e295f573
derdeche <foued.derdeche@gmail.com>
2023-07-05 12:01:16 +0200

    ajout autorisation de boire

--- Gaulois1.sql
+++ Gaulois1.sql
@@ -86,3 +86,7 @@ WHERE aT.id_potion != 1
 
 INSERT INTO personnage (nom_personnage, id_specialite, id_lieu, adresse_personnage)
 VALUES ('Champdeblix',12,6,'Hantassion')
+
+/*B.B. Autorisez Bonemine à boire de la potion magique, elle est jalouse d'Iélosubmarine...*/
+INSERT INTO autoriser_boire (id_personnage, id_potion)
+VALUES (12,1)
